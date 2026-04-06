e0b1d4a6bc1027ccc9a426b500dc99e9ef95fe8e
fss4 <f.spencer.stubbs@gmail.com>
2026-04-06 00:00:57 -0400

    Added a function that turns bold, italic, or code markdown text into appropriately labeled TextNode instances.

--- src/textnode.py
+++ src/textnode.py
@@ -24,3 +24,21 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type.value != "text":
+            new_nodes.append(node)
+        if delimiter not in node.text:
+            raise Exception("delimiter {delimiter} not found. Invalid markdown syntax.")
+        new_nodes_text = (node.text).split(f"{delimiter}")
+        check = 0
+        for new in new_nodes_text:
+ #NEED TO DEAL WIITH THE CASE WHEN THE NODE IS AT THE FRONT OF THE SENTENCE. THERE IS A EMPTY STRING THAT NEEDS TO BE DEALT WITH CORRECTLY.
+            if check % 2 == 1:
+                new_nodes.append(TextNode(new, text_type))
+            else:
+                if new:
+                    new_nodes.append(TextNode(new,TextType.TEXT))
+            check += 1            
+    return new_nodes
